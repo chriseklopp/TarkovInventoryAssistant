cc930e9ce3b6cc233b82e1904ad41752ae645356
Chris Klopp <chriseklopp@gmail.com>
2023-02-27 22:39:32 -0500

    Added utility file and namespace for objects that might be useful. Added c++ -like pair.

---
+++ TarkovInventoryAssistant_Server/source/ServerCore/Utilities.cs
@@ -0,0 +1,25 @@
+﻿namespace TarkovInventoryAssistant_Server
+{
+
+    /*
+     This namespace contains helper classes that may be useful elsewhere in the code.  
+     */
+    namespace Utilities
+    {
+        // Because Tuple isn't mutable!
+        public class Pair<T1, T2>
+        {
+            public Pair(T1 first, T2 second)
+            {
+                First = first;
+                Second = second;
+            }
+
+            public T1 First { get; set; }
+            public T2 Second { get; set; }
+        }
+
+
+    }
+
+}
