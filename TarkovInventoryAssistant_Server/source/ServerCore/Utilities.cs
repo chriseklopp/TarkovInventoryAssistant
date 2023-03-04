b30565d4b8879ab3bd2c7a368575284590e6a5d3
Chris Klopp <chriseklopp@gmail.com>
2023-03-03 19:27:23 -0500

    Added TotalValueCounter class to the utilities namespace. This object is used to track the total value of a list of detection results.

--- TarkovInventoryAssistant_Server/source/ServerCore/Utilities.cs
+++ TarkovInventoryAssistant_Server/source/ServerCore/Utilities.cs
@@ -1,4 +1,7 @@
-﻿namespace TarkovInventoryAssistant_Server
+﻿using Interop;
+using System.Text;
+
+namespace TarkovInventoryAssistant_Server
 {
 
     /*
@@ -18,6 +21,62 @@
             public T1 First { get; set; }
             public T2 Second { get; set; }
         }
+
+        // Object used to track the total value of DetectionResults.
+        public class TotalValueCounter
+        {
+            public TotalValueCounter()
+            {
+                m_unitValueMap = new Dictionary<string, int>();
+            }
+
+            public void addResultEntry(DetectionResultMarshal drm) {
+
+                string unit;
+                int value;
+                if (drm.sellOnFlea)
+                {
+                    int nullIndex = Array.IndexOf(drm.fleaUnit, (byte)0);
+                    string fleaUnit = Encoding.UTF8.GetString(drm.fleaUnit, 0, nullIndex);
+                    unit = fleaUnit;
+                    value = drm.fleaprice;
+                }
+                else
+                {
+                    int nullIndex = Array.IndexOf(drm.traderUnit, (byte)0);
+                    string traderunit = Encoding.UTF8.GetString(drm.traderUnit, 0, nullIndex);
+                    unit = traderunit;
+                    value = drm.fleaprice;
+                }
+
+                if (m_unitValueMap.ContainsKey(unit))
+                {
+                    m_unitValueMap[unit] += value;
+                }
+                else
+                {
+                    m_unitValueMap[unit] = value;
+                }
+            
+            }
+            // Formats units and values into a nicely displayable string.
+            public string getValueString()
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (string unit in m_unitValueMap.Keys)
+                {
+                    sb.Append(unit + m_unitValueMap[unit].ToString("N0") + "  ");
+                }
+
+                return sb.ToString();
+            }
+
+
+            // Unit : cumulative value
+            private Dictionary<string, int> m_unitValueMap;
+
+        }
+
 
 
     }
